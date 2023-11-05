69e679117922a3568af3da2496a2760d3923deb6
S M Piyas Mahamude Alif <piyasmahmudealif@gmail.com>
2023-11-05 23:26:22 +0600

    create added food api

--- index.js
+++ index.js
@@ -7,12 +7,69 @@ const port = process.env.PORT || 5020;
 
 
 // middlewares-------------------------------------------------------------------------------------------->>>>>
-app.use(cors())
+app.use(cors({
+    origin:["http://localhost:5173"],
+    credentials:true
+}))
 app.use(express.json())
+require('dotenv').config()
 // middlewares-------------------------------------------------------------------------------------------->>>>>
 
 
 // ----------------------------------------------------MongoDB-------------------------------------------->>>>>
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.frg7rqf.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const addedFoodCollection = client.db('ShareAbite').collection('addedFoodCollection');
+
+
+    app.post('/addFood',async(req,res)=>{
+        const data = req.body;
+        const result = await addedFoodCollection.insertOne(data)
+        res.send(result)
+    })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 // ----------------------------------------------------MongoDB-------------------------------------------->>>>>
 
